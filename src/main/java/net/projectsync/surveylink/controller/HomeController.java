bc1368c0ee85a8fb27f97396a0637e59b05d78ca
Mohan <mohan.rvce@outlook.com>
2021-08-01 17:29:29 +0000

    initial commit

--- src/main/java/net/projectsync/surveylink/controller/HomeController.java
+++ src/main/java/net/projectsync/surveylink/controller/HomeController.java
@@ -25,7 +25,9 @@ public class HomeController {
 	@RequestMapping(value = "/contact", method = RequestMethod.GET)
 	public String contact() {
 		return "Website under maintenance maode. Try again later...";
-	}	
+	}
+
+	// test	
 
 }
 
